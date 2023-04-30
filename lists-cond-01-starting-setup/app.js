01921b7e71217fe4b21927366ae50f2ec484beb9
mishra2000 <50536949+mishra2000@users.noreply.github.com>
2023-04-30 13:10:36 +0530

    using v-else, read description for explaination (#26)

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -1,7 +1,18 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return { 
+      goals: [],
+      enteredValue: ''
+    };
   },
+  methods: {
+    addGoal(){
+      console.log(this.enteredValue)
+      this.goals.push(this.enteredValue);
+      this.enteredValue = ''
+      console.log(this.goals)
+    }
+  }
 });
 
 app.mount('#user-goals');
